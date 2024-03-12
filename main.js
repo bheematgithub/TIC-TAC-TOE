688b0d7f7ca352b8e0237e7fdd8e895454153de6
bheematgithub <bhimashiplc@gmail.com>
2024-03-13 00:45:35 +0530

    bootstrapApplied
    Dark theme bootstrap apllied version

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const winPatterns = [
   [0, 4, 8],
   [1, 4, 7],
   [2, 5, 8],
-  [2, 4, 6],
+  [2, 4, 6],  
   [3, 4, 5],
   [6, 7, 8]
 ];
@@ -40,13 +40,12 @@ const checkWinner = () => {
     if (pos1 !== "" && pos2 !== "" && pos3 !== "") {
       if (pos1 === pos2 && pos3 === pos2) {
         Winner(pos1)
+      } else if (count == 9) {
+        msg.innerText = `Match Draw!`;
+        msgCtr.classList.remove("hide");
       }
     }
   });
-  if (count == 9) {
-    msg.innerText = `Match Draw!`;
-    msgCtr.classList.remove("hide");
-  }
 }
 
 const Winner = (win) => {
